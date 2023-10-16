7007c4fe50e1f5c676644675f98280dde9f20128
NguyenMinh Tuan <nguyenminhtuan28072003@gmail.com>
2023-10-16 10:49:40 +0700

    update

--- THUCHANH2/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-for word in line.split():
-#         if word.isnumeric() and b<=int(word) and int(word)<=a:
-#             continue
-#         List.append(line)
-
-# for i in sorted(List):
-#     print(i,end="")

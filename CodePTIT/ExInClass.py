f478feb23aa8092ad849eed68b1aefa1bb82fe99
NguyenMinh Tuan <nguyenminhtuan28072003@gmail.com>
2023-10-30 22:49:29 +0700

    update

--- CodePTIT/ExInClass.py
+++ CodePTIT/ExInClass.py
@@ -1,21 +1,4 @@
-#In ra so chia het cho 5 va 9
-# for i in range(1100,3901): 
-#     if i%5==0 and i%9==0:
-#         print(i,end=" ")
-# import re
-# s=input()
-# a=re.findall('\d+',s)
-# sum=0
-# for i in a:
-#     sum+=len(i)
-# print(sum)
-
-
-# list_subjects=[("English",88),("Science",90),("Maths",97),("Social sciences",82)]
-
-# list_sorted=sorted(list_subjects,key=lambda x:x[1])
-# print(list_sorted)
-
-from Giaithua import giaithua
-number=int(input())
-print(giaithua(number))
+class SV:
+    def __init__(self,name,):
+        
+        pass
